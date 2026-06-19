5e403a220366e84c59412ba3ece17e4624441cc9
Sachin-Pathak-1 <sachinpathak9930@gmail.com>
2026-06-19 10:25:00 +0530

    feat: add recipe card components

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export const RecipeCard = ({ meal }) => {
+    return (
+        <Link to={`/recipe/${meal.idMeal}`}>
+            <div className='group relative bg-gray-900 rounded-xl shadow-black/50 overflow-hidden transform transition duration-500 cursor-pointer border border-gray-800 hover:shadow-blue-600/50'>
+                <div className='absolute inset-0 rounded-xl border-2 border-transparent group-hover:border-blue-500/80 transition duration-500'>
+                </div>
+                <div className='flex justify-center items-center p-5'>
+                    <img src={meal?.strMealThumb} alt="img" className='h-60 w-60 rounded-xl border-yellow-400 border transition duration-500 group-hover:scale-105' />
+                </div>
+                <div className="p-2 text-center">
+                    <h1 className="text-xl pb-3 font-bold text-gray-100 group-hover:text-blue-400 transition duration-300">{meal?.strMeal}</h1>
+                </div>
+            </div>
+        </Link>
+    )
+}
---
+++ src/components/RecipeCardMini.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export const RecipeCardMini = ({ meal }) => {
+    return (
+        <Link to={`/recipe/${meal.idMeal}`}>
+            <div key={meal.idMeal} className="px-10 flex justify-center">
+                <div className='group relative bg-gray-900 rounded-xl shadow-black/50 overflow-hidden transform transition duration-500 cursor-pointer border border-gray-800 hover:shadow-blue-600/50 mb-5'>
+                    <div className='absolute inset-0 rounded-xl border-2 border-transparent group-hover:border-blue-500/80 transition duration-500'>
+                    </div>
+                    <div className='flex justify-center items-center p-5'>
+                        <img src={meal?.strMealThumb} alt="img" className='h-30 w-30 rounded-xl border-yellow-400 border transition duration-500 group-hover:scale-105' />
+                    </div>
+                </div>
+            </div>
+        </Link>
+    )
+}
