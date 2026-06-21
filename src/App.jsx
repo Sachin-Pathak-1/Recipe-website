e4c9ea2874826b0f358e3789f181f47d5bb81124
Sachin-Pathak-1 <sachinpathak9930@gmail.com>
2026-06-21 14:35:00 +0530

    feat: wire recipe app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Routes ,Route } from "react-router-dom";
+import { HomeView } from "./components/HomeView";
+import {RecipeDetailView} from "./components/RecipeDetailView";
+import { Layout } from "./pages/Layout";
+import { SearchView } from "./components/SearchView";
+import { SelectionView } from "./components/SelectionView";
+import { CountrySelector } from "./components/CountrySelector";
 
-const App = () => {
+
+function App() {
+
   return (
-    <div>App</div>
-  )
+    <Layout>
+      <Routes>
+        <Route index element={<HomeView />} />
+        <Route path="/recipe/:id" element={<RecipeDetailView />} />
+        <Route path="/select/country" element={<CountrySelector/>}/>
+        <Route path="/search/:input" element={<SearchView />} />
+        <Route path="/select/:filterType/:valueType" element={<SelectionView />} />
+    </Routes>
+    </Layout>
+  );
 }
 
 export default App
