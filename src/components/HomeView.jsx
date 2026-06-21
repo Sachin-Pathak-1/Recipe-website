2355b291ef05774aaba7c10f637ccdec303f75e4
Sachin-Pathak-1 <sachinpathak9930@gmail.com>
2026-06-21 15:10:00 +0530

    feat: compose home page view

---
+++ src/components/HomeView.jsx
@@ -0,0 +1,22 @@
+import { CategorySelection } from "./CategorySelection";
+import { RecipeSlider } from "./RecipeSlider";
+import { TrendingRecipe } from "./TrendingRecipe";
+import { API_URL } from "./useFetch";
+
+export function HomeView() {
+    return (
+        <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+            <RecipeSlider
+                title="Staff Curated Picks"
+                fetchUrl={`${API_URL}search.php?f=c`}
+            />
+
+            <TrendingRecipe
+                title="Quick & Easy Meals"
+                fetchUrl={`${API_URL}filter.php?a=Canadian`}
+            />
+
+            <CategorySelection />
+        </div>
+    );
+}
